a875d46aee5227e6652ce463907625cf1a215c47
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-09 22:07:17 +0530

    First commit of custom exception program

--- src/CustomException.java
+++ src/CustomException.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
-// TODO: Create class InvalidAgeException extends Exception
-// Create a constructor that accepts a message string and passes it to super(message)
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
 
 public class CustomException {
     
-    // TODO: Create a static method validate(int age) that throws InvalidAgeException
-    // Check if age < 18, throw new InvalidAgeException("Not eligible to vote")
-    // Else print "Eligible to vote"
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    
-        // TODO: Read age
-        
+        int age = Integer.parseInt(sc.nextLine());
+
+        try {
+            if (age < 18) {
+                throw new InvalidAgeException("Not eligible to vote");
+            } else {
+                System.out.print("Eligible to vote");
+            }
+        } catch (InvalidAgeException e) {
+            System.out.print("Exception: " + e.getMessage());
+        }
+    }
 }
